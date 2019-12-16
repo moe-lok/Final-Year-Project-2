49a9942c488f8613299071e5cb904c2487ff2ecd
lokman <muhammad24lokman@gmail.com>
2019-12-16 10:49:41 +0800

    deploy to heroku attempt

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI: "mongodb+srv://moe_test:12345@cluster0-txkxx.mongodb.net/barcode_app?retryWrites=true&w=majority", 
-    secretOrKey: "secret"
+    secretOrKey: "secret" || process.env.SECRET,
 };
+//mongodb://heroku_q50qln74:gve175eq2n4sn69uo9mgv4k15m@ds253388.mlab.com:53388/heroku_q50qln74
--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const users = require("./routes/api/users");
 const entriesRouter = require("./routes/api/entries")
 const confMachRouter = require("./routes/api/confmachine")
 const bonRouter = require("./routes/api/bonderEntries")
+const path = require("path")
+require("dotenv").config()
 
 const app = express();
 app.use(cors())
 app.use(express.json());
 // DB Config
-const db = require("./config/keys").mongoURI;
+const db = require("./config/keys").mongoURI || process.env.MONGODB_URI;
 // Connect to MongoDB
 mongoose
   .connect(
@@ -31,6 +33,12 @@ app.use("/api/users", users);
 app.use("/api/entries", entriesRouter)
 app.use("/api/confmach", confMachRouter)
 app.use("/api/bon", bonRouter)
+
+app.use(express.static(path.join(__dirname,"client", "build")))
+
+app.get("*", (req, res) =>{
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} ...!`));
